ae7115857c11177ad8547ca7d8addc4f979cb489
Anna Eklund <anna.eklund@yahoo.se>
2025-08-19 19:19:35 +0200

    Add mock timeline

---
+++ src/components/molecules/Timeline.tsx
@@ -0,0 +1,13 @@
+function Timeline() {
+  return (
+    <section>
+      <p>0:10</p>
+      <div className="mock-timeline">
+        <div className="mock-timeline-inner"></div>
+      </div>
+      <p>3:10</p>
+    </section>
+  );
+}
+
+export default Timeline;
