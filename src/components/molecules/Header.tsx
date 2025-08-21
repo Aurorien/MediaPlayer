3a3f2b91267c6fd073c74136a61675839f1048bd
Anna Eklund <anna.eklund@yahoo.se>
2025-08-21 23:21:23 +0200

    Add icon styling

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -3,7 +3,7 @@ import "./Header.css";
 function Header() {
   return (
     <header>
-      <span className="material-symbols-outlined">arrow_back_ios</span>
+      <span className="material-symbols-outlined back">arrow_back_ios</span>
       <h1>Playlist</h1>
       <span className="material-symbols-outlined">more_vert</span>
     </header>
